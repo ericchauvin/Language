937ee82f0ceb727d5c9fd649684cb7c2fc8a60b8
Eric Chauvin <chauvineric4@gmail.com>
2020-06-28 13:54:16 -0600

    Create URLFile.java

---
+++ URLFile.java
@@ -0,0 +1,13 @@
+// Copyright Eric Chauvin 2020.
+
+
+  // This is similar to one page.
+  // This can be anything at a specific URL.  It can
+  // be an html file, a cgi script page, a jpeg file,
+  // or anything.
+  
+
+public class URLFile
+  {
+
+  }
