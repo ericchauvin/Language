ba739c1bb5d0e9ee3b7cf6a7e9a23ee3dd834ec2
Eric Chauvin <chauvineric4@gmail.com>
2020-06-28 13:52:24 -0600

    Create WordDictionary.java

---
+++ WordDictionary.java
@@ -0,0 +1,15 @@
+// Copyright Eric Chauvin 2020.
+
+
+// This will be a word dictionary and the most
+// popular words will have indexes with the smallest
+// numbers.  So like the word 'the' would be at
+// index zero.  Same thing for Spanish, in its
+// own dictionary.
+
+
+public class WordDictionary
+  {
+
+  }
+
