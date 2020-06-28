b7e43bdc34a0746bbdde5975aeb467dcebd83286
Eric Chauvin <chauvineric4@gmail.com>
2020-06-28 13:53:24 -0600

    Create SentenceDictionary.java

---
+++ SentenceDictionary.java
@@ -0,0 +1,15 @@
+// Copyright Eric Chauvin 2020.
+
+
+// A dictionary of sentences.
+// This will eventually be a tree structure.
+// But first it's just a list of sentences, along
+// with the URL it came from.
+
+
+
+public class SentenceDictionary
+  {
+
+  }
+
