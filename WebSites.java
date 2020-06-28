71325655b5d261d98e6f2675061d1cea03de5c55
Eric Chauvin <chauvineric4@gmail.com>
2020-06-28 13:55:07 -0600

    Create WebSites.java

---
+++ WebSites.java
@@ -0,0 +1,38 @@
+// Copyright Eric Chauvin 2020.
+
+
+  // This is a collection of WebSite objects.
+
+
+
+public class WebSites
+  {
+  private MainApp mApp;
+
+  private WebSites()
+    {
+    }
+
+
+  public WebSites( MainApp appToUse )
+    {
+    mApp = appToUse;
+
+    }
+
+
+
+  public void processWebSites()
+    {
+// https://durangoherald.com/
+// https://www.durangogov.org/
+// https://www.gilacountyaz.gov/
+// https://www.paysonaz.gov/
+// https://www.co.laplata.co.us/
+// https://www.paysonroundup.com/news/
+// https://www.azcentral.com/
+
+    }
+
+
+  }
