b35195b6422b5fe9a96d62878f2fb01ee0582c25
Eric Chauvin <chauvineric4@gmail.com>
2020-07-15 13:13:32 -0600

    Update URLClient.java

--- URLClient.java
+++ URLClient.java
@@ -13,6 +13,7 @@ public class URLClient implements Runnable
   private MainApp mApp;
   private String fileName = "";
   private String URLToGet = "";
+  private String contentType = "";
 
 
 
@@ -36,20 +37,16 @@ public class URLClient implements Runnable
   @Override
   public void run()
     {
-    getURLToFile();
+    // getContentType: text/html; charset=utf-8
+    // UTF8 doesn't have to be ASCII.
+
+    // if contentType is ...
+    getURLToAsciiFile();
     }
 
 
- *   <li>{@code getContentEncoding}
- *   <li>{@code getContentLength}
- *   <li>{@code getContentType}
- *   <li>{@code getDate}
- *   <li>{@code getExpiration}
- *   <li>{@code getLastModifed}
 
-
-
-  private void getURLToFile()
+  private void getURLToAsciiFile()
     {
     try
     {
@@ -63,20 +60,52 @@ public class URLClient implements Runnable
                       new InputStreamReader(
                           uConnect.getInputStream()));
 
+    mApp.showStatusAsync( "Getting: " + URLToGet );
+    mApp.showStatusAsync( "getContentEncoding: " +
+               uConnect.getContentEncoding() );
+
+    // mApp.showStatusAsync( "getContentLength: " +
+    //           uConnect.getContentLength() );
+
+    mApp.showStatusAsync( "getContentType: " +
+               uConnect.getContentType() );
+
+    // mApp.showStatusAsync( "getDate: " +
+    //           uConnect.getDate() );
+
+    // mApp.showStatusAsync( "getExpiration: " +
+    //           uConnect.getExpiration() );
+
+    // mApp.showStatusAsync( "getLastModified: " +
+    //           uConnect.getLastModified() );
+
+
     StringBuilder sBuilder = new StringBuilder();
     for( int count = 0; count < 1000000; count++ )
       {
       String line = in.readLine();
       if( line == null )
         break;
+
+      if( line.length() == 0 )
+        continue;
  
-      sBuilder.append( line );
+      // Things like two-slash comments in JavaScript
+      // need to have the linefeed character to
+      // end the line.
+      sBuilder.append( line + "\n" );
       // mApp.showStatusAsync( "Got line: " + line );
       }
 
     in.close();
 
-    StrA StrToWrite = new StrA( sBuilder.toString());
+    // Charset iso 8859-1
+    // Windows-1252.
+    // Characters from 128 to 159 used for symbols.
+
+    String asciiS = getAsciiOnly( sBuilder.toString());
+    StrA StrToWrite = new StrA( asciiS );
+
     StrA fileToWrite = new StrA( fileName );
 
     // mApp.showStatusAsync( "File StrA: " + StrToWrite );
@@ -93,6 +122,23 @@ public class URLClient implements Runnable
       mApp.showStatusAsync( "Exception in URLClient.getURLToFile()." );
       mApp.showStatusAsync( e.getMessage() );
       }
+    }
+
+
+
+  private String getAsciiOnly( String in )
+    {
+    StringBuilder sBuilder = new StringBuilder();
+    final int last = in.length();
+    for( int count = 0; count < last; count++ )
+      {
+      char testC = in.charAt( count );
+      if( testC <= (char)126 )
+        sBuilder.append( testC );
+
+      }
+
+    return sBuilder.toString();
     }
 
 
