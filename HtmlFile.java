3cfef1b1ff9b3e5c6ba16e7163d8ebddf65bf629
Eric Chauvin <chauvineric4@gmail.com>
2020-07-16 12:37:36 -0600

    Update HtmlFile.java

--- HtmlFile.java
+++ HtmlFile.java
@@ -2,11 +2,33 @@
 
 
 
+
 public class HtmlFile
   {
   private MainApp mApp;
-  private StrA textS = StrA.Empty;
+  private StrA htmlS = StrA.Empty;
   private StrA scriptS = StrA.Empty;
+  private StrA title = StrA.Empty;
+
+
+  private static final StrA tagTitleStart = new
+                                      StrA( "title" );
+
+  private static final StrA tagTitleEnd = new
+                                      StrA( "/title" );
+
+  private static final StrA tagAnchorStart = new
+                                         StrA( "a" );
+
+  private static final StrA tagAnchorEnd = new
+                                         StrA( "/a" );
+
+  private static final StrA tagHeadStart = new
+                                       StrA( "head" );
+
+  private static final StrA tagHeadEnd = new
+                                       StrA( "/head" );
+
 
   private HtmlFile()
     {
@@ -22,7 +44,7 @@ public class HtmlFile
   public boolean processFile( StrA fileName )
     {
     if( fileName.length() == 0 )
-      return false;
+      return true; // false;
 
     mApp.showStatusAsync( "\n\nReading: " + fileName );
 
@@ -34,29 +56,22 @@ public class HtmlFile
     if( fileS.length() == 0 )
       {
       mApp.showStatusAsync( "File length zero.\n" + fileName );
-      return false;
+      return true; // false;
       }
 
     // mApp.showStatusAsync( "HTML file: " + fileS );
-    getScriptAndText( fileS );
+    getScriptAndHtml( fileS );
+    processHtml();
 
-/*
-    StrArray lines = fileS.splitChar( '\n' );
-    final int last = lines.length();
-    for( int count = 0; count < last; count++ )
-      {
-      StrA line = lines.getStrAt( count );
-      // mApp.showStatusAsync( "Line: " + line );
-      }
-*/
     return true;
     }
 
 
 
-  private void getScriptAndText( StrA in )
+
+  private void getScriptAndHtml( StrA in )
     {
-    textS = StrA.Empty;
+    htmlS = StrA.Empty;
     scriptS = StrA.Empty;
 
     StrABld textBld = new StrABld( in.length() );
@@ -65,6 +80,7 @@ public class HtmlFile
     StrArray splitS = in.splitStrA( new StrA(
                        "<script" ));
 
+    // At zero, before the first script tag.
     textBld.appendStrA( splitS.getStrAt( 0 ));
 
     final int last = splitS.length();
@@ -74,12 +90,17 @@ public class HtmlFile
       StrArray lineSplit = line.splitStrA( new StrA(
                        "</script>" ));
 
-      if( lineSplit.length() < 2 )
+      int splitfields = lineSplit.length();
+      if( splitfields > 2 )
         {
-// ======= It just has the first part.  Nothing
-// following it.
+        mApp.showStatusAsync( "Script has more than one end tag." );
+        mApp.showStatusAsync( "line: " + line );
+        return;
+        }
 
-        mApp.showStatusAsync( "Script doesn't have an end tag." );
+      if( splitfields == 0 )
+        {
+        mApp.showStatusAsync( "Script has nothing in it." );
         mApp.showStatusAsync( "line: " + line );
         return;
         }
@@ -88,15 +109,117 @@ public class HtmlFile
       scriptBld.appendStrA( lineSplit.getStrAt( 0 ));
       scriptBld.appendStrA( new StrA( "</script>" ));
 
-      textBld.appendStrA( lineSplit.getStrAt( 1 ));
+      if( splitfields > 1 )
+        textBld.appendStrA( lineSplit.getStrAt( 1 ));
+   
       }
 
-
-    textS = textBld.toStrA();
+    htmlS = textBld.toStrA();
     scriptS = scriptBld.toStrA();
 
-    // mApp.showStatusAsync( "\n\ntextS: " + textS );
-    mApp.showStatusAsync( "\n\nscriptS: " + scriptS );
+    // mApp.showStatusAsync( "\n\nhtmlS: " + htmlS );
+    // mApp.showStatusAsync( "\n\nscriptS: " + scriptS );
+    }
+
+
+  private void processHtml()
+    {
+    boolean isInsideHeader = false;
+    boolean isInsideTitle = false;
+    boolean isInsideAnchor = false;
+
+
+    // The link tag is for style sheets.
+
+    StrArray tagParts = htmlS.splitChar( '<' );
+    final int last = tagParts.length();
+    // mApp.showStatusAsync( "Before first tag: " + tagParts.getStrAt( 0 ));
+
+    for( int count = 1; count < last; count++ )
+      {
+      StrA line = tagParts.getStrAt( count );
+      StrArray lineParts = line.splitChar( '>' );
+      final int lastPart = lineParts.length();
+      if( lastPart == 0 )
+        {
+        mApp.showStatusAsync( "The tag doesn't have any parts." );
+        mApp.showStatusAsync( "line: " + line );
+        return;
+        }
+
+      StrA tag = lineParts.getStrAt( 0 );
+ 
+      // tag: a href="//radio.foxnews.com/podcast" data-omtr-intcmp="hp1navpod"
+      // tag: /a
+
+
+      // mApp.showStatusAsync( "tag: " + tag );
+      StrArray tagAttr = tag.splitChar( ' ' );
+      final int lastAttr = tagAttr.length();
+      if( lastAttr == 0 )
+        {
+        mApp.showStatusAsync( "lastAttr is zero for the tag." );
+        mApp.showStatusAsync( "tag: " + tag );
+        return;
+        }
+
+      StrA tagName = tagAttr.getStrAt( 0 );
+      tagName = tagName.toLowerCase();
+      // mApp.showStatusAsync( "\n\ntagName: " + tagName );
+
+      if( tagName.equalTo( tagHeadStart ))
+        isInsideHeader = true;
+
+      if( tagName.equalTo( tagHeadEnd ))
+        isInsideHeader = false;
+
+      if( tagName.equalTo( tagTitleStart ))
+        isInsideTitle = true;
+
+      if( tagName.equalTo( tagTitleEnd ))
+        isInsideTitle = false;
+
+      // Inside the div tag there can be a title tag
+      // for that division.
+
+      if( isInsideTitle && isInsideHeader )
+        {
+        if( lastPart < 2 )
+          {
+          mApp.showStatusAsync( "Title has no text: " +
+                         line );
+          return;
+          }
+
+        title = lineParts.getStrAt( 1 );
+        mApp.showStatusAsync( "Title: " + title );
+        }
+
+      if( tagName.equalTo( tagAnchorStart ))
+        {
+        isInsideAnchor = true;
+        for( int countA = 1; countA < lastAttr; countA++ )
+          {
+          mApp.showStatusAsync( "Tag attribues: " +
+                        tagAttr.getStrAt( countA ));
+          }
+        }
+
+
+      if( tagName.equalTo( tagAnchorEnd ))
+        isInsideAnchor = false;
+
+
+/*
+      for( int countL = 1; countL < lastPart; countL++ )
+        {
+        mApp.showStatusAsync( "Outside tags: " +
+                         lineParts.getStrAt( countL ));
+
+        }
+*/
+
+      }
 
     }
 
