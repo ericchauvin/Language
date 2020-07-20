4ff1913536c973a2d13fc8b09460fdcdadd1d5c2
Eric Chauvin <chauvineric4@gmail.com>
2020-07-20 13:41:04 -0600

    Update UTF8StrA.java

--- UTF8StrA.java
+++ UTF8StrA.java
@@ -17,6 +17,7 @@ public class UTF8StrA
   {
 
 
+
   public static void doTest( MainApp mApp )
     {
     StrABld sBld = new StrABld( 1024 * 64);
@@ -31,16 +32,10 @@ public class UTF8StrA
 
     StrA testS = sBld.toStrA();
     final int max = testS.length();
-    byte[] buf = strAToBytes( testS );
-    if( !isValidUTF8( mApp, buf, 1000000000 ))
-      {
-      mApp.showStatusAsync( "Not valid UTF8." );
-      return;
-      }
 
-    // byte[] buf = UTF8Strings.strAToBytes( testS );
-    StrA result = bytesToStrA( buf, 1000000000 );
-    // StrA result = UTF8Strings.bytesToStrA( buf, 1000000000 );
+    byte[] buf = strAToBytes( testS );
+
+    StrA result = bytesToStrA( mApp, buf, 1000000000 );
 
     // for( int count = 0; count < max; count++ )
       // {
@@ -58,6 +53,7 @@ public class UTF8StrA
       mApp.showStatusAsync( "It failed the test." );
 
     }
+
 
 
 
@@ -85,8 +81,19 @@ public class UTF8StrA
     for( int count = 0; count < max; count++ )
       {
       char fullChar = in.charAt( count );
-      if( fullChar >= 0xD800 ) // High Surrogates
+      // Two 16 bits values make up the whole
+      // character for these surrogate values.
+      // The high 16 bits and the low 16 bits.
+      // High surrogate is 0xD800 to 0xDBFF.
+      // Low surrogate is 0xDC00 to 0xDFFF.
+      if( fullChar >= 0xD800 )
         fullChar = ' '; 
+
+// From Wikipedia:
+// 1 U+0000 U+007F 0xxxxxxx   
+// 2 U+0080 U+07FF 110xxxxx 10xxxxxx  
+// 3 U+0800 U+FFFF 1110xxxx 10xxxxxx 10xxxxxx 
+// 4 U+10000 U+10FFFF[18] 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
 
       if( fullChar <= 0x7F )
         {
@@ -104,14 +111,10 @@ public class UTF8StrA
         }
 
 
-      //  7   U+007F   0xxxxxxx
-      // 11   U+07FF   110xxxxx   10xxxxxx
-      // 16   U+FFFF   1110xxxx   10xxxxxx   10xxxxxx
-      if( (fullChar > 0x7F) && (fullChar <= 0x7FF) )
+// 2 U+0080 U+07FF 110xxxxx 10xxxxxx  
+
+      if( (fullChar >= 0x80) && (fullChar <= 0x7FF) )
         {
-        // Notice that this cast (byte) from 
-        // characters to bytes doesn't involve
-        // characters over 0x7F.
         // Bottom 6 bits.
         byte smallByte = (byte)(fullChar & 0x3F); 
         // Big 5 bits.
@@ -132,8 +135,12 @@ public class UTF8StrA
         where++;
         }
 
-      // 16   U+FFFF   1110xxxx   10xxxxxx   10xxxxxx
-      if( fullChar > 0x7FF )
+
+// This would encode things like High Surrogate
+// values like any other character.
+
+// 3 U+0800 U+FFFF 1110xxxx 10xxxxxx 10xxxxxx 
+      if( fullChar >= 0x800 ) // && (fullChar <= 0xFFFF  )
         {
         byte byte3 = (byte)(fullChar & 0x3F);
         byte byte2 = (byte)((fullChar >> 6) & 0x3F);
@@ -165,7 +172,8 @@ public class UTF8StrA
 
 
 
-  public static StrA bytesToStrA( byte[] in,
+  public static StrA bytesToStrA( MainApp mApp,
+                                  byte[] in,
                                   int maxLen )
     {
     try
@@ -187,6 +195,8 @@ public class UTF8StrA
     boolean isInside = false;
     for( int count = 0; count < last; count++ )
       {
+      // This character is from a byte, so it can't
+      // be more than 0xFF.
       char c = (char)in[count];
       if( (c & 0x80) == 0 )
         {
@@ -224,44 +234,78 @@ public class UTF8StrA
       sBld.appendChar( Markers.End );
 
     StrA markedS = sBld.toStrA();
-    StrA result = convertMarked( markedS );
-    return result;
+    return convertMarked( mApp, markedS );
     }
     catch( Exception e )
       {
-      // mApp.showStatusAsync( "Exception in UTF8StrA.bytesToStrA(): " + e.getMessage() );
+      mApp.showStatusAsync( "Exception in UTF8StrA.bytesToStrA(): " + e.getMessage() );
       return StrA.Empty;
       }
     }
 
 
 
-  private static StrA convertMarked( // MainApp mApp, 
+// From Wikipedia:
+// 1 U+0000 U+007F 0xxxxxxx   
+// 2 U+0080 U+07FF 110xxxxx 10xxxxxx  
+// 3 U+0800 U+FFFF 1110xxxx 10xxxxxx 10xxxxxx 
+// 4 U+10000 U+10FFFF[18] 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+
+  private static StrA convertMarked( MainApp mApp, 
                                      StrA in )
     {
     if( in.length() == 0 )
       return StrA.Empty;
 
+    // Begin = (char)0x2702;
+    // End = (char)0x2703;
+
     StrArray splitS = in.splitChar( Markers.Begin );
     final int last = splitS.length();
-    StrABld sBld = new StrABld( last );
+    StrABld sBld = new StrABld( in.length());
 
     // Add what's before the first Begin marker.
-    sBld.appendStrA( splitS.getStrAt( 0 ));
+    StrA lowChars = splitS.getStrAt( 0 );
+    // Remove the end marker.
+    lowChars = Markers.removeAllMarkers( lowChars );
+    sBld.appendStrA( lowChars );
 
     for( int count = 1; count < last; count++ )
       {
       StrA line = splitS.getStrAt( count );
+      if( 1 != Markers.countMarkers( line ))
+        {
+        mApp.showStatusAsync( "The number of markers is not one." );
+        return StrA.Empty;
+        }
+
       StrArray splitLine = line.splitChar(
                                        Markers.End );
 
       StrA highChars = splitLine.getStrAt( 0 );
-      StrA lowChars = StrA.Empty;
+      // mApp.showStatusAsync( "highChars: " + highChars );
+
+      final int hLength = highChars.length();
+
+      if( hLength == 0 )
+        {
+        mApp.showStatusAsync( "Sequence was length zero." );
+        highChars = StrA.Empty;
+        }
+
+      if( hLength < 2 )
+        {
+        mApp.showStatusAsync( "Single char: " + highChars );
+        highChars = StrA.Empty;
+        }
+    
+      lowChars = StrA.Empty;
       if( splitLine.length() > 1 )
         lowChars = splitLine.getStrAt( 1 );
 
-      char fullChar = sequenceToChar( highChars );
+      char fullChar = sequenceToChar( mApp, highChars );
       sBld.appendChar( fullChar );
+
       sBld.appendStrA( lowChars );
       }
 
@@ -270,22 +314,9 @@ public class UTF8StrA
 
 
 
-  private static char sequenceToChar( // MainApp mApp,
+  private static char sequenceToChar( MainApp mApp,
                                       StrA in )
     {
-    final int max = in.length();
-    if( max < 2 )
-      {
-      // mApp.showStatusAsync( "Sequence was less than two characters." );
-      return ' ';
-      }
-    
-    if( max > 4 )
-      {
-      // mApp.showStatusAsync( "max > 4." );
-      return ' ';
-      }
-
     //  7   U+007F   0xxxxxxx
     // 11   U+07FF   110xxxxx   10xxxxxx
     // 16   U+FFFF   1110xxxx   10xxxxxx   10xxxxxx
@@ -297,32 +328,38 @@ public class UTF8StrA
     // D 1101
     // E 1110
     // F 1111 
-    char first = in.charAt( 0 );
-    char second = in.charAt( 1 );
-    // It should be a continuing byte.
-    if( (second & 0xC0) != 0x80 )
-      {
-      // mApp.showStatusAsync( "Second is not a continuing byte." );
-      return ' ';
-      }
+    
+    final int max = in.length();
+    if( max < 2 )
+      return '_';    
 
+    char first = in.charAt( 0 );
     // A beginning byte is either 110xxxxx or
     // 1110xxxx or 11110xxx.
     if( (first & 0xC0) != 0xC0 )
       {
-      // mApp.showStatusAsync( "First is not a beginning byte." );
-      return ' ';
+      mApp.showStatusAsync( "First is not a beginning byte." );
+      return '_';
+      }
+
+    char second = in.charAt( 1 );
+    // It should be a continuing byte.
+    if( (second & 0xC0) != 0x80 )
+      {
+      mApp.showStatusAsync( "Second is not a continuing byte." );
+      return '_';
       }
 
     // Two bytes.
     if( (first & 0xE0) == 0xC0 )
       {
+      // mApp.showStatusAsync( "Got a two byte sequence: " + in );
       // It is a 2 byte sequence.
       // Starts with 110xxxxx.
       if( max != 2 )
         {
-        // mApp.showStatusAsync( "Max != 2." );
-        return ' ';
+        mApp.showStatusAsync( "max != 2: " + in );
+        return '_';
         }
 
       char fullChar = (char)(first & 0x1F);
@@ -340,237 +377,19 @@ public class UTF8StrA
     // Three bytes.
     if( (first & 0xF0) == 0xE0 )
       {
+      // mApp.showStatusAsync( "Got a three byte sequence: " + in );
       // It is a 3 byte sequence.
       if( max != 3 )
         {
-        // mApp.showStatusAsync( "max != 3." );
-        return ' ';
-        }
-
-      char third = in.charAt( 2 );
-      if( (third & 0xC0) != 0x80 )
-      {
-      // mApp.showStatusAsync( "third is not a continuing byte." );
-      return ' ';
-      }
-
-      char fullChar = (char)(first & 0x0F);
-      char byte2 = (char)(second & 0x3F);
-      char byte3 = (char)(third & 0x3F);
-      fullChar = (char)(fullChar << 12);
-      fullChar |= byte2 << 6;
-      fullChar |= byte3;
-      if( fullChar >= 0xD800 ) // High Surrogates
-        {
-        // mApp.showStatusAsync( "fullChar >= 0xD800." );
-        return ' ';
-        }
-
-      return fullChar;
-      }
-
-    // Four bytes.
-    if( (first & 0xF0) == 0xF0 )
-      {
-      // This doesn't convert the longer unusual 4
-      // byte sequences.  It just does the Basic
-      // Multilingual Plane.
-
-      if( max != 4 )
-        {
-        // mApp.showStatusAsync( "max != 4." );
-        return ' ';
-        }
-
-      // mApp.showStatusAsync( "It was four bytes." );
-      return '_';
-      }
-
-    // mApp.showStatusAsync( "It was none of the above." );
-    return '_';
-    }
-
-
-
-  public static boolean isValidUTF8( MainApp mApp,
-                                  byte[] in,
-                                  int maxLen )
-    {
-    try
-    {
-    if( in == null )
-      return false;
-
-    if( in.length == 0 )
-      return false;
-
-    if( in[0] == 0 )
-      return false;
-
-    if( maxLen > in.length )
-      maxLen = in.length;
-
-    final int last = maxLen;
-    StrABld sBld = new StrABld( last + (1024 * 8));
-    boolean isInside = false;
-    for( int count = 0; count < last; count++ )
-      {
-      char c = (char)in[count];
-      if( (c & 0x80) == 0 )
-        {
-        // It is regular ASCII.
-        if( isInside )
-          sBld.appendChar( Markers.End );
-
-        isInside = false;
-        sBld.appendChar( c );
-        continue;
-        }
-
-      // If it is a beginning byte.
-      if( (c & 0xC0) == 0xC0 )
-        {
-        if( isInside )
-          {
-          // There can be a sequence of high bytes
-          // with no ASCII characters in between.
-          sBld.appendChar( Markers.End );
-          sBld.appendChar( Markers.Begin );
-          }
-        }
-
-      if( !isInside )
-        {
-        sBld.appendChar( Markers.Begin );
-        isInside = true;
-        }
-
-      sBld.appendChar( c );
-      }
-
-    if( isInside )
-      sBld.appendChar( Markers.End );
-
-    StrA markedS = sBld.toStrA();
-    return convertMarkedTest( mApp, markedS );
-    }
-    catch( Exception e )
-      {
-      mApp.showStatusAsync( "Exception in UTF8StrA.isValidUTF8(): " + e.getMessage() );
-      return false;
-      }
-    }
-
-
-
-  private static boolean convertMarkedTest(
-                                     MainApp mApp, 
-                                     StrA in )
-    {
-    if( in.length() == 0 )
-      return false;
-
-    StrArray splitS = in.splitChar( Markers.Begin );
-    final int last = splitS.length();
-    for( int count = 1; count < last; count++ )
-      {
-      StrA line = splitS.getStrAt( count );
-      StrArray splitLine = line.splitChar(
-                                       Markers.End );
-
-      StrA highChars = splitLine.getStrAt( 0 );
-      StrA lowChars = StrA.Empty;
-      if( splitLine.length() > 1 )
-        lowChars = splitLine.getStrAt( 1 );
-
-      if( !sequenceToCharTest( mApp, highChars ))
-        return false;
-
-      }
-
-    return true;
-    }
-
-
-
-  private static boolean sequenceToCharTest(
-                                      MainApp mApp,
-                                      StrA in )
-    {
-    final int max = in.length();
-    if( max < 2 )
-      {
-      mApp.showStatusAsync( "Sequence was less than two characters." );
-      return false;
-      }
-    
-    if( max > 4 )
-      {
-      mApp.showStatusAsync( "max > 4." );
-      return false;
-      }
-
-    //  7   U+007F   0xxxxxxx
-    // 11   U+07FF   110xxxxx   10xxxxxx
-    // 16   U+FFFF   1110xxxx   10xxxxxx   10xxxxxx
-    // 4 bytes       11110xxx  10xxxxxx  10xxxxxx  10xxxxxx
-
-    // A 1010
-    // B 1011
-    // C 1100
-    // D 1101
-    // E 1110
-    // F 1111 
-    char first = in.charAt( 0 );
-    char second = in.charAt( 1 );
-    // It should be a continuing byte.
-    if( (second & 0xC0) != 0x80 )
-      {
-      mApp.showStatusAsync( "Second is not a continuing byte." );
-      return false;
-      }
-
-    // A beginning byte is either 110xxxxx or
-    // 1110xxxx or 11110xxx.
-    if( (first & 0xC0) != 0xC0 )
-      {
-      mApp.showStatusAsync( "First is not a beginning byte." );
-      return false;
-      }
-
-    // Two bytes.
-    if( (first & 0xE0) == 0xC0 )
-      {
-      // It is a 2 byte sequence.
-      // Starts with 110xxxxx.
-      if( max != 2 )
-        {
-        mApp.showStatusAsync( "Max != 2." );
-        return false;
-        }
-
-      char fullChar = (char)(first & 0x1F);
-      fullChar = (char)(fullChar << 6);
-      char byte2 = (char)(second & 0x3F);
-      fullChar |= byte2;
-      return true;
-      }
-
-    // Three bytes.
-    if( (first & 0xF0) == 0xE0 )
-      {
-      // It is a 3 byte sequence.
-      if( max != 3 )
-        {
-        mApp.showStatusAsync( "max != 3." );
-        return false;
+        mApp.showStatusAsync( "max != 3: " + in );
+        return '_';
         }
 
       char third = in.charAt( 2 );
       if( (third & 0xC0) != 0x80 )
       {
       mApp.showStatusAsync( "third is not a continuing byte." );
-      return false;
+      return '_';
       }
 
       char fullChar = (char)(first & 0x0F);
@@ -582,14 +401,14 @@ public class UTF8StrA
       if( fullChar >= 0xD800 ) // High Surrogates
         {
         mApp.showStatusAsync( "fullChar >= 0xD800." );
-        return false;
+        return '_';
         }
 
-      return true;
+      return fullChar;
       }
 
     // Four bytes.
-    if( (first & 0xF0) == 0xF0 )
+    if( (first & 0xF8) == 0xF0 )
       {
       // This doesn't convert the longer unusual 4
       // byte sequences.  It just does the Basic
@@ -597,82 +416,19 @@ public class UTF8StrA
 
       if( max != 4 )
         {
-        mApp.showStatusAsync( "max != 4." );
-        return false;
+        mApp.showStatusAsync( "max != 4. max is: " + max );
+        return '_';
         }
 
       mApp.showStatusAsync( "It was four bytes." );
-      return true;
+      return '_';
       }
 
     mApp.showStatusAsync( "It was none of the above." );
-    return false;
+    return '_';
     }
 
 
-
-
-/*
-  private static StrA convertAsWindowsCodePage( StrA in )
-    {
-    final int last = in.length();
-    char[] result = new char[last];
-    for( int count = 0; count < last; count++ )
-      {
-      result[count] = replaceWinCodePageChar( 
-                                  in.charAt( count ));
-      }
-
-    return new StrA( result );
-    }
-
-
-
-  public static char replaceWinCodePageChar( char in )
-    {
-    if( in == '\n' )
-      return '\n';
-
-    if( in == '\t' )
-      return '\t';
-
-    if( in < ' ' )
-      return ' ';
-
-    if( in > 0xFF )
-      return Markers.ShowOddChar; // '_';
-
-    // 126 is the tilde character.
-    // 127 is delete.
-    if( in >= 127 )
-      {
-      return Markers.ShowOddChar;
-
-      // if( (in >= 232) && (in <= 235))
-        // return 'e';
-
-      // if( in == 169 ) // copyright.
-        // return 'c';
-
-      // if( in == 174 ) // rights symbol
-        // return 'r';
-
-      // return '_';
-
-      // C1 Controls and Latin-1 Supplement (0080 00FF)
-      // Latin Extended-A (0100 017F)
-      // Latin Extended-B (0180 024F)
-
-    // 161 is upside down exclamation.
-    // 209 is capital N like el niNa.
-    // 232 through 235 is e.
-    //    mApp.showStatusAsync( "\n\n" );
-
-      }
-    
-    return Markers.ShowOddChar;
-    }
-*/
 
 
   }
